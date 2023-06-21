d258a8974b3d24038b6c9f2253757afbcac1f877
Taratrove <132413958+Taratrove@users.noreply.github.com>
2023-06-21 15:03:35 +0700

    Add files via upload

---
+++ Nguyễn Tiến Dũng-62130312-KTGK2/js/main.js
@@ -0,0 +1,9 @@
+bane = document.querySelector('#bane');
+
+bane.onmouseover= function () {
+    bane.style.color = 'yellow';
+}
+
+bane.onmouseout = function () {
+    bane.style.color = 'rgb(54, 51, 51)';
+}
